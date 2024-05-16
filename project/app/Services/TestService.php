8d4201b57c6176d9a3f1a0da841601ea1c953788
Xenan <k.kirill@meta.studio>
2024-05-16 21:44:32 +0700

    feat(test controller service): added getResultByTestAnswers function

--- project/app/Services/TestService.php
+++ project/app/Services/TestService.php
@@ -2,7 +2,9 @@
 
 namespace App\Services;
 
+use App\Models\Answer;
 use App\Models\Question;
+use App\Models\Result;
 use Illuminate\Support\Collection;
 
 class TestService
@@ -10,5 +12,15 @@ class TestService
     public function getTestQuestions(): Collection
     {
         return Question::query()->with('answers')->orderBy('position')->get();
+    }
+
+    public function getResultByTestAnswers(array $answersIds): Result
+    {
+        $answersResultsIds = Answer::query()->whereIn('id', $answersIds)
+            ->with('result')->get()->pluck('result.id')->toArray();
+
+        $answerId = array_search(max($answersResultsIds), $answersResultsIds);
+
+        return Result::query()->find($answerId);
     }
 }
