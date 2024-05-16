4ed94a064febd8bdbcaf0540cfe11d88fdde8347
Xenan <k.kirill@meta.studio>
2024-05-16 21:45:05 +0700

    feat: test result route

--- project/app/Http/Controllers/TestController.php
+++ project/app/Http/Controllers/TestController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\TestResultRequest;
 use App\Http\Resources\QuestionResource;
+use App\Http\Resources\ResultResource;
 use App\Services\TestService;
 use Illuminate\Http\JsonResponse;
 
@@ -18,5 +20,13 @@ class TestController extends Controller
     public function getTest(): JsonResponse
     {
         return response()->json(QuestionResource::collection($this->service->getTestQuestions()));
+    }
+
+    public function getTestResult(TestResultRequest $request): JsonResponse
+    {
+        $answersIds = $request->validated()['answers'];
+        $result = $this->service->getResultByTestAnswers($answersIds);
+
+        return response()->json(new ResultResource($result));
     }
 }
--- project/routes/api.php
+++ project/routes/api.php
@@ -20,3 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/test', [TestController::class, 'getTest']);
+Route::patch('/test_result', [TestController::class, 'getTestResult']);
