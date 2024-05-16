16e859d4b67e6d86bc1391881a6cba101d167417
Xenan <k.kirill@meta.studio>
2024-05-16 20:44:19 +0700

    fix(questions migration): nullable position field

--- project/database/migrations/2024_05_16_131046_create_questions_table.php
+++ project/database/migrations/2024_05_16_131046_create_questions_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('questions', function (Blueprint $table) {
             $table->id();
             $table->text('text')->nullable();
-            $table->unsignedInteger('position');
+            $table->unsignedInteger('position')->nullable();
             $table->timestamps();
         });
     }
