f4b19cfc3f0e0812bc55bb65e5f182119174d07f
Xenan <k.kirill@meta.studio>
2024-05-16 21:06:33 +0700

    feat: answer relations

--- project/app/Models/Answer.php
+++ project/app/Models/Answer.php
@@ -4,10 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Answer extends Model
 {
     use HasFactory;
 
     protected $guarded = [];
+
+    public function question(): HasOne
+    {
+        return $this->hasOne(Question::class);
+    }
+
+    public function answer(): HasOne
+    {
+        return $this->hasOne(Answer::class);
+    }
 }
