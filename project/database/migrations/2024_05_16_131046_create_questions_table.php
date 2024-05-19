64a5ffcb12f0027c351a7fbc230ca3c0320abde7
Xenan <k.kirill@meta.studio>
2024-05-19 16:58:28 +0700

    fix: remove position field from question table

--- project/database/migrations/2024_05_16_131046_create_questions_table.php
+++ project/database/migrations/2024_05_16_131046_create_questions_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('questions', function (Blueprint $table) {
             $table->id();
             $table->text('text')->nullable();
-            $table->unsignedInteger('position')->nullable();
             $table->timestamps();
         });
     }
